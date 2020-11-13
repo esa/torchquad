e38e1ab7cc86b27c15748312656096282450ccf9
Pablo Gómez <contact@pablo-gomez.net>
2020-11-13 14:40:16 +0100

    - Added plots

---
+++ torchquad/plots/plot_runtime.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+
+def plot_runtime(evals, runtime, labels, dpi=150):
+    """Plots runtime vs fevals
+
+    Args:
+        evals (list of np.array): number of evaluations, for each method a np.array of fevals]
+        runtime (list of np.array): runtime for evals
+        labels (list): method names
+        dpi (int, optional): plot dpi. Defaults to 150.
+    """
+    fig = plt.figure(dpi=dpi)
+    for evals_item, rt, label in zip(evals, runtime, labels):
+        plt.semilogy(evals_item, rt, label=label)
+    plt.legend()
+    plt.xlabel("Number of evaluations")
+    plt.ylabel("Runtime [s]")
