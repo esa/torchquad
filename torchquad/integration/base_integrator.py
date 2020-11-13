70e905523a71e2b37ce1f3df9478698422a2bd17
Pablo Gómez <contact@pablo-gomez.net>
2020-11-13 16:47:09 +0100

    - Added eval to baseintegrator to track evals - Added Simpson1D - Added Simpson1D test

--- torchquad/integration/base_integrator.py
+++ torchquad/integration/base_integrator.py
@@ -15,10 +15,19 @@ class BaseIntegrator:
     _nr_of_fevals = None
 
     def __init__(self):
-        pass
+        self._nr_of_fevals = 0
 
     def integrate(self):
         raise (
             NotImplementedError("This is an abstract base class. Should not be called.")
         )
+
+    def _eval(self, points):
+        """Evaluates the function at the passed points and updates nr_of_evals
+
+        Args:
+            points (torch.tensor): integration points
+        """
+        self._nr_of_fevals += len(points)
+        return self._fn(points)
 
