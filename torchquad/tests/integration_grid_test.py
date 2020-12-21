e5a3192e678daac38fab495516829cd90e766036
Pablo Gómez <contact@pablo-gomez.net>
2020-12-21 12:04:47 +0100

    Added integration grid test

--- torchquad/tests/integration_grid_test.py
+++ torchquad/tests/integration_grid_test.py
@@ -3,27 +3,44 @@ import sys
 sys.path.append("../")
 
 from integration.integration_grid import IntegrationGrid
+import torch
 
 
 def test_integration_grid():
-    """Tests the integration grid 
-    """#TODO add more
+    """Tests the integration grid in integration.integration_grid 
+    """
 
-    # Generate a grid in different dimension with different N
-    # on different domains and check if
+    # Generate a grid in different dimension with different N on different domains
     N = 10
-    l_bound, r_bound = 0, 1
-    integration_domain = [[l_bound, r_bound]]
-    grid = IntegrationGrid(N, integration_grid)
+    eps = 1e-8
+    integration_domain = [[0, 1]]
+    grid = IntegrationGrid(N, integration_domain)
 
+    # test if  numbers of points is correct
     assert grid._N == N
-    # mesh width is correct
-    assert grid.h == 1/N
-    # numbers of points is correct
     assert len(grid.points) == N
-    # all points are inside
-    assert grid.point[:, 0] >= l_bound
-    assert grid.point[:, 0] <= r_bound
+    for dim in range(len(integration_domain)):
+        # test if mesh width is correct
+        assert torch.abs(grid.h[dim] - 1 / (N - 1)) < eps
+        # test if all points are inside
+        assert torch.all(grid.points[:, dim] >= integration_domain[dim][0])
+        assert torch.all(grid.points[:, dim] <= integration_domain[dim][1])
+
+    N = 27
+    integration_domain = [[0, 2], [-2, 1], [0.5, 1]]
+    grid = IntegrationGrid(N, integration_domain)
+
+    # test if  numbers of points is correct
+    assert grid._N == int(N ** (1 / len(integration_domain)))
+    assert len(grid.points) == N
+    for dim in range(len(integration_domain)):
+        domain_width = integration_domain[dim][1] - integration_domain[dim][0]
+        # test if mesh width is correct
+        assert torch.abs(grid.h[dim] - domain_width / (grid._N - 1)) < eps
+        # test if all points are inside
+        assert torch.all(grid.points[:, dim] >= integration_domain[dim][0])
+        assert torch.all(grid.points[:, dim] <= integration_domain[dim][1])
+
 
 test_integration_grid()
 
