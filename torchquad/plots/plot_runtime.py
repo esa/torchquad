fa7831e4be9eac3e2ae00c98d37defd8935411f6
GabrieleMeoni <gabriele.meoni@esa.int>
2021-01-29 18:21:15 +0100

    Fixed bug in Benchmark_notebook. Added possibility to specify a different y axis name in plot_runtime.

--- torchquad/plots/plot_runtime.py
+++ torchquad/plots/plot_runtime.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def plot_runtime(evals, runtime, labels, dpi=150):
+def plot_runtime(evals, runtime, labels, dpi=150, y_axis_name="Runtime [s]"):
     """Plots runtime vs fevals
 
     Args:
@@ -9,10 +9,11 @@ def plot_runtime(evals, runtime, labels, dpi=150):
         runtime (list of np.array): runtime for evals
         labels (list): method names
         dpi (int, optional): plot dpi. Defaults to 150.
+        y_axis_name (str, optional): name for y axis. Deafult to ""Runtime [s]""
     """
     fig = plt.figure(dpi=dpi)
     for evals_item, rt, label in zip(evals, runtime, labels):
         plt.semilogy(evals_item, rt, label=label)
     plt.legend(fontsize=6)
     plt.xlabel("Number of evaluations")
-    plt.ylabel("Runtime [s]")
+    plt.ylabel(y_axis_name)
