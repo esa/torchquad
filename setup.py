b3a3fc42f3967ab9f750200885aeb605bc9b7324
Pablo Gómez <contact@pablo-gomez.net>
2021-02-05 10:44:00 +0100

    Adjusted version in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchquad",
-    version="0.1.0",
+    version="0.1.1",
     description="Package providing torch-based numerical integration methods.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
