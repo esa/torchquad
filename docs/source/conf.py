fef0bfa5d6a96508f1cd09201132096b2160de47
GabrieleMeoni <gabriele.meoni@esa.int>
2021-02-19 13:41:26 +0100

    Added logo to the left bar. Removed logo from index page.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,11 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "sphinx_rtd_theme"
 
+html_logo = "Torchquad_logo_white_background.png"
+html_theme_options = {
+    'logo_only': True,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
