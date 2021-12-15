75a9fc7fe0b4cd581c8923e61cd98fa5dc9e8390
FHof <ga84muv@mytum.de>
2021-12-15 11:34:01 +0100

    Add a jit_integrate method to NewtonCotes and MonteCarlo

--- torchquad/integration/monte_carlo.py
+++ torchquad/integration/monte_carlo.py
@@ -52,6 +52,111 @@ class MonteCarlo(BaseIntegrator):
         function_values, self._nr_of_fevals = self.evaluate_integrand(fn, sample_points)
         return self.calculate_result(function_values, integration_domain)
 
+    def jit_integrate(
+        self, dim, N=1000, integration_domain=None, seed=None, rng=None, backend="torch"
+    ):
+        """Create an integrate function where the performance-relevant steps except the integrand evaluation are JIT compiled.
+        Use this method only if the integrand cannot be compiled.
+        The compilation happens when the function is executed the first time.
+        With PyTorch, return values of different integrands passed to the compiled function must all have the same format, e.g. precision.
+
+        Args:
+            dim (int): Dimensionality of the integration domain.
+            N (int, optional): Number of sample points to use for the integration. Defaults to 1000.
+            integration_domain (list or backend tensor, optional): Integration domain, e.g. [[-1,1],[0,1]]. Defaults to [-1,1]^dim. It also determines the numerical backend if possible.
+            seed (int, optional): Random number generation seed to the sampling point creation, only set if provided. Defaults to None.
+            rng (RNG, optional): An initialised RNG; this must be specified with Tensorflow and omitted with JAX
+            backend (string, optional): Numerical backend. This argument is ignored if the backend can be inferred from integration_domain. Defaults to "torch".
+
+        Returns:
+            function(fn, integration_domain): JIT compiled integrate function where all parameters except the integrand and domain are fixed
+        """
+        self._check_inputs(dim=dim, N=N, integration_domain=integration_domain)
+        integration_domain = _setup_integration_domain(dim, integration_domain, backend)
+        backend = infer_backend(integration_domain)
+        if backend in ["tensorflow", "jax"]:
+            # Tensorflow and JAX automatically recompile functions if
+            # the parameters change
+            if backend == "tensorflow":
+                if not hasattr(self, "_tf_jit_calculate_sample_points"):
+                    import tensorflow as tf
+
+                    self._tf_jit_calculate_sample_points = tf.function(
+                        self.calculate_sample_points, jit_compile=True
+                    )
+                    self._tf_jit_calculate_result = tf.function(
+                        self.calculate_result, jit_compile=True
+                    )
+                jit_calculate_sample_points = self._tf_jit_calculate_sample_points
+                jit_calculate_result = self._tf_jit_calculate_result
+            elif backend == "jax":
+                if not hasattr(self, "_jax_jit_calculate_sample_points"):
+                    import jax
+
+                    self._jax_jit_calculate_sample_points = jax.jit(
+                        self.calculate_sample_points, static_argnames=["N"]
+                    )
+                    self._jax_jit_calculate_result = jax.jit(
+                        self.calculate_result, static_argnames=["dim", "n_per_dim"]
+                    )
+                jit_calculate_sample_points = self._jax_jit_calculate_sample_points
+                jit_calculate_result = self._jax_jit_calculate_result
+
+            def compiled_integrate(fn, integration_domain):
+                sample_points = jit_calculate_sample_points(
+                    N, integration_domain, seed, rng
+                )
+                function_values, _ = self.evaluate_integrand(fn, sample_points)
+                return jit_calculate_result(function_values, integration_domain)
+
+            return compiled_integrate
+
+        elif backend == "torch":
+            # Torch requires explicit tracing with example inputs.
+            def do_compile(example_integrand):
+                import torch
+
+                # Define traceable first and third steps
+                def step1(integration_domain):
+                    return self.calculate_sample_points(
+                        N, integration_domain, seed, rng
+                    )
+
+                step3 = self.calculate_result
+
+                # Trace the first step (which is non-deterministic)
+                step1 = torch.jit.trace(step1, (integration_domain,), check_trace=False)
+
+                # Get example input for the third step
+                sample_points = step1(integration_domain)
+                function_values, _ = self.evaluate_integrand(
+                    example_integrand, sample_points
+                )
+
+                # Trace the third step
+                step3 = torch.jit.trace(step3, (function_values, integration_domain))
+
+                # Define a compiled integrate function
+                def compiled_integrate(fn, integration_domain):
+                    sample_points = step1(integration_domain)
+                    function_values, _ = self.evaluate_integrand(fn, sample_points)
+                    return step3(function_values, integration_domain)
+
+                return compiled_integrate
+
+            # Do the compilation when the returned function is executed the
+            # first time
+            compiled_func = [None]
+
+            def lazy_compiled_integrate(fn, integration_domain):
+                if compiled_func[0] is None:
+                    compiled_func[0] = do_compile(fn)
+                return compiled_func[0](fn, integration_domain)
+
+            return lazy_compiled_integrate
+
+        raise ValueError(f"Compilation not implemented for backend {backend}")
+
     def calculate_sample_points(self, N, integration_domain, seed=None, rng=None):
         """Calculate random points for the integrand evaluation
 
--- torchquad/integration/newton_cotes.py
+++ torchquad/integration/newton_cotes.py
@@ -1,4 +1,5 @@
 from loguru import logger
+from autoray import infer_backend
 
 from .base_integrator import BaseIntegrator
 from .integration_grid import IntegrationGrid
@@ -32,6 +33,118 @@ class NewtonCotes(BaseIntegrator):
         self._nr_of_fevals = num_points
 
         return self.calculate_result(function_values, dim, n_per_dim, hs)
+
+    def jit_integrate(self, dim, N=None, integration_domain=None, backend="torch"):
+        """Create an integrate function where the performance-relevant steps except the integrand evaluation are JIT compiled.
+        Use this method only if the integrand cannot be compiled.
+        The compilation happens when the function is executed the first time.
+        With PyTorch, return values of different integrands passed to the compiled function must all have the same format, e.g. precision.
+
+        Args:
+            dim (int): Dimensionality of the integration domain.
+            N (int, optional): Total number of sample points to use for the integration. See the integrate method documentation for more details.
+            integration_domain (list or backend tensor, optional): Integration domain, e.g. [[-1,1],[0,1]]. Defaults to [-1,1]^dim. It also determines the numerical backend if possible.
+            backend (string, optional): Numerical backend. This argument is ignored if the backend can be inferred from integration_domain. Defaults to "torch".
+
+        Returns:
+            function(fn, integration_domain): JIT compiled integrate function where all parameters except the integrand and domain are fixed
+        """
+        # If N is None, use the minimal required number of points per dimension
+        if N is None:
+            N = self._get_minimal_N(dim)
+
+        integration_domain = _setup_integration_domain(dim, integration_domain, backend)
+        self._check_inputs(dim=dim, N=N, integration_domain=integration_domain)
+        backend = infer_backend(integration_domain)
+        if backend in ["tensorflow", "jax"]:
+            # Tensorflow and JAX automatically recompile functions if
+            # the parameters change
+            if backend == "tensorflow":
+                if not hasattr(self, "_tf_jit_calculate_grid"):
+                    import tensorflow as tf
+
+                    self._tf_jit_calculate_grid = tf.function(
+                        self.calculate_grid, jit_compile=True
+                    )
+                    self._tf_jit_calculate_result = tf.function(
+                        self.calculate_result, jit_compile=True
+                    )
+                jit_calculate_grid = self._tf_jit_calculate_grid
+                jit_calculate_result = self._tf_jit_calculate_result
+            elif backend == "jax":
+                if not hasattr(self, "_jax_jit_calculate_grid"):
+                    import jax
+
+                    self._jax_jit_calculate_grid = jax.jit(
+                        self.calculate_grid, static_argnames=["N"]
+                    )
+                    self._jax_jit_calculate_result = jax.jit(
+                        self.calculate_result, static_argnames=["dim", "n_per_dim"]
+                    )
+                jit_calculate_grid = self._jax_jit_calculate_grid
+                jit_calculate_result = self._jax_jit_calculate_result
+
+            def compiled_integrate(fn, integration_domain):
+                grid_points, hs, n_per_dim = jit_calculate_grid(N, integration_domain)
+                function_values, _ = self.evaluate_integrand(fn, grid_points)
+                return jit_calculate_result(function_values, dim, int(n_per_dim), hs)
+
+            return compiled_integrate
+
+        elif backend == "torch":
+            # Torch requires explicit tracing with example inputs.
+            def do_compile(example_integrand):
+                import torch
+
+                # Define traceable first and third steps
+                def step1(integration_domain):
+                    grid_points, hs, n_per_dim = self.calculate_grid(
+                        N, integration_domain
+                    )
+                    return (
+                        grid_points,
+                        hs,
+                        torch.Tensor([n_per_dim]),
+                    )  # n_per_dim is constant
+
+                def step3(function_values, hs):
+                    dim = int(integration_domain.shape[0])
+                    return self.calculate_result(function_values, dim, n_per_dim, hs)
+
+                # Trace the first step
+                step1 = torch.jit.trace(step1, (integration_domain,))
+
+                # Get example input for the third step
+                grid_points, hs, n_per_dim = step1(integration_domain)
+                n_per_dim = int(n_per_dim)
+                function_values, _ = self.evaluate_integrand(
+                    example_integrand, grid_points
+                )
+
+                # Trace the third step
+                step3 = torch.jit.trace(step3, (function_values, hs))
+
+                # Define a compiled integrate function
+                def compiled_integrate(fn, integration_domain):
+                    grid_points, hs, _ = step1(integration_domain)
+                    function_values, _ = self.evaluate_integrand(fn, grid_points)
+                    result = step3(function_values, hs)
+                    return result
+
+                return compiled_integrate
+
+            # Do the compilation when the returned function is executed the
+            # first time
+            compiled_func = [None]
+
+            def lazy_compiled_integrate(fn, integration_domain):
+                if compiled_func[0] is None:
+                    compiled_func[0] = do_compile(fn)
+                return compiled_func[0](fn, integration_domain)
+
+            return lazy_compiled_integrate
+
+        raise ValueError(f"Compilation not implemented for backend {backend}")
 
     def calculate_grid(self, N, integration_domain):
         """Calculate grid points, widths and N per dim
