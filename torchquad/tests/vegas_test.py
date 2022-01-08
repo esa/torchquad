dc664a7a7ce1bc2729e7a96f94f29b131a1ae39d
FHof <ga84muv@mytum.de>
2022-01-08 16:01:16 +0100

    Extend vegas_test.py for other numerical backends
    Currently torch and numpy are supported

--- torchquad/tests/vegas_test.py
+++ torchquad/tests/vegas_test.py
@@ -7,18 +7,14 @@ import cProfile
 import pstats
 
 from integration.vegas import VEGAS
-from utils.enable_cuda import enable_cuda
-from utils.set_precision import set_precision
-from utils.set_log_level import set_log_level
-from integration_test_utils import compute_integration_test_errors
+from integration_test_utils import (
+    compute_integration_test_errors,
+    setup_test_for_backend,
+)
 
 
-def test_integrate():
-    """Tests the integrate function in integration.VEGAS."""
-    set_log_level("INFO")
-    enable_cuda()
-    set_precision("double")
-
+def _run_vegas_tests(backend, _precision):
+    """Test the integrate function in integration.VEGAS for the given backend."""
     vegas = VEGAS()
 
     # 1D Tests
@@ -28,7 +24,7 @@ def test_integrate():
         {"N": N, "dim": 1, "seed": 0},
         dim=1,
         use_complex=False,
-        backend="torch",
+        backend=backend,
     )
     print("1D VEGAS Test: Passed N =", N, "\n", "Errors: ", errors)
     for error in errors[:3]:
@@ -47,7 +43,7 @@ def test_integrate():
         {"N": N, "dim": 3, "seed": 0},
         dim=3,
         use_complex=False,
-        backend="torch",
+        backend=backend,
     )
     print("3D VEGAS Test: Passed N =", N, "\n", "Errors: ", errors)
     for error in errors:
@@ -60,19 +56,25 @@ def test_integrate():
         {"N": N, "dim": 10, "seed": 0},
         dim=10,
         use_complex=False,
-        backend="torch",
+        backend=backend,
     )
     print("10D VEGAS Test: Passed N =", N, "\n", "Errors: ", errors)
     for error in errors:
         assert error < 12.5
 
 
+test_integrate_numpy = setup_test_for_backend(_run_vegas_tests, "numpy", "double")
+test_integrate_torch = setup_test_for_backend(_run_vegas_tests, "torch", "double")
+
+
 if __name__ == "__main__":
     # used to run this test individually
+    test_integrate_numpy()
+
     profiler = cProfile.Profile()
     profiler.enable()
     start = timeit.default_timer()
-    test_integrate()
+    test_integrate_torch()
     profiler.disable()
     stats = pstats.Stats(profiler).sort_stats("tottime")
     stats.print_stats()
