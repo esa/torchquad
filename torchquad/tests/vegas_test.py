49f8f3d125ab25406a01632fc15dfaedfdddda05
FHof <ga84muv@mytum.de>
2022-01-11 12:59:14 +0100

    Add VEGAS+ accuracy tests

--- torchquad/tests/vegas_test.py
+++ torchquad/tests/vegas_test.py
@@ -2,6 +2,8 @@ import sys
 
 sys.path.append("../")
 
+from autoray import numpy as anp
+from autoray import to_backend_dtype, astype
 import timeit
 import cProfile
 import pstats
@@ -13,9 +15,24 @@ from integration_test_utils import (
 )
 
 
-def _run_vegas_tests(backend, _precision):
-    """Test the integrate function in integration.VEGAS for the given backend."""
+def _run_example_integrations(backend, precision):
+    """Test the integrate method in VEGAS for the given backend and example test functions using compute_integration_test_errors"""
+    print(f"Testing VEGAS+ with example functions with {backend}, {precision}")
     vegas = VEGAS()
+
+    test_zero_integral = False
+    if test_zero_integral:
+        # Test with integrand which is zero everywhere
+        # (not yet supported in VEGAS+)
+        integral = vegas.integrate(
+            lambda x: x[:, 0] * 0.0,
+            2,
+            N=10000,
+            integration_domain=[[0.0, 3.0]] * 2,
+            seed=0,
+            backend=backend,
+        )
+        assert anp.abs(integral) < 5e-3
 
     # 1D Tests
     N = 10000
@@ -63,6 +80,89 @@ def _run_vegas_tests(backend, _precision):
         assert error < 12.5
 
 
+def _run_vegas_accuracy_checks(backend, precision):
+    """Test VEGAS+ with special peak integrands where it should be significantly more accurate than MonteCarlo"""
+    print(f"Testing VEGAS+ accuracy with {backend}, {precision}")
+    dtype_name = {"float": "float32", "double": "float64"}[precision]
+    dtype = to_backend_dtype(dtype_name, like=backend)
+    integrator = VEGAS()
+
+    print("Integrating a function with a single peak")
+    integration_domain = anp.array(
+        [[1.0, 5.0], [-4.0, 4.0], [2.0, 6.0]], dtype=dtype, like=backend
+    )
+    dim = integration_domain.shape[0]
+
+    def integrand_hypercube_peak(x):
+        """An integrand which is close to zero everywhere except in a hypercube of volume 1."""
+        # A product corresponds to logical And
+        in_cube = anp.prod((x >= 3.0) * (x < 4.0), axis=1)
+        # Add 0.01 since VEGAS+ does not yet support integrands which evaluate
+        # to zero for all passed points
+        return astype(in_cube, dtype_name) + 0.001
+
+    reference_integral = (
+        anp.prod(integration_domain[:, 1] - integration_domain[:, 0]) * 0.001 + 1.0
+    )
+
+    # Use multiple seeds to reduce luck
+    for seed in [0, 1, 2, 3, 41317]:
+        integral = integrator.integrate(
+            integrand_hypercube_peak,
+            dim,
+            N=30000,
+            integration_domain=integration_domain,
+            seed=seed,
+        )
+        assert anp.abs(integral - reference_integral) < 0.03
+
+    print("Integrating a function with peaks on the diagonal")
+    peak_distance = 100.0
+    integration_domain = anp.array(
+        [[1.0, 1.0 + peak_distance], [-4.0, -4.0 + peak_distance]],
+        dtype=dtype,
+        like=backend,
+    )
+    dim = 2
+
+    def integrand_diagonal_peaks(x):
+        """An integrand which is close to zero everywhere except two corners of the integration domain."""
+        a = anp.exp(anp.sum(integration_domain[:, 0] - x, axis=1))
+        b = anp.exp(anp.sum(x - integration_domain[:, 1], axis=1))
+        return a + b
+
+    # If the integration domain is [r_1, r_1 + c]x[r_2, r_2 + c]
+    # for some numbers r_1, r_2,
+    # the integral of integrand_diagonal_peaks is the integral of
+    # exp(-x_1) exp(-x_2) + exp(x_1 - c) exp(x_2 - c) over x in [0, c]^2.
+    # indefinite integral:
+    # F(x) = exp(-x_1) exp(-x_2) + exp(x_1 - c) exp(x_2 - c)
+    # definite integral:
+    # F((c,c)) - F((c,0)) - F((0,c)) + F((0,0)) = 2 - 4 exp(-c) + 2 exp(-2c)
+    reference_integral = (
+        2.0
+        - 4.0 * anp.exp(-peak_distance, like="numpy")
+        + 2.0 * anp.exp(-2.0 * peak_distance, like="numpy")
+    )
+
+    # Use multiple seeds to reduce luck
+    for seed in [0, 1, 2, 3, 41317]:
+        integral = integrator.integrate(
+            integrand_diagonal_peaks,
+            dim,
+            N=30000,
+            integration_domain=integration_domain,
+            seed=seed,
+        )
+        assert anp.abs(integral - reference_integral) < 0.03
+
+
+def _run_vegas_tests(backend, precision):
+    """Test if VEGAS+ works with example functions and is accurate as expected"""
+    _run_vegas_accuracy_checks(backend, precision)
+    _run_example_integrations(backend, precision)
+
+
 test_integrate_numpy = setup_test_for_backend(_run_vegas_tests, "numpy", "double")
 test_integrate_torch = setup_test_for_backend(_run_vegas_tests, "torch", "double")
 
@@ -71,12 +171,16 @@ if __name__ == "__main__":
     # used to run this test individually
     test_integrate_numpy()
 
-    profiler = cProfile.Profile()
-    profiler.enable()
-    start = timeit.default_timer()
-    test_integrate_torch()
-    profiler.disable()
-    stats = pstats.Stats(profiler).sort_stats("tottime")
-    stats.print_stats()
-    stop = timeit.default_timer()
-    print("Test ran for ", stop - start, " seconds.")
+    profile_torch = False
+    if profile_torch:
+        profiler = cProfile.Profile()
+        profiler.enable()
+        start = timeit.default_timer()
+        test_integrate_torch()
+        profiler.disable()
+        stats = pstats.Stats(profiler).sort_stats("tottime")
+        stats.print_stats()
+        stop = timeit.default_timer()
+        print("Test ran for ", stop - start, " seconds.")
+    else:
+        test_integrate_torch()
