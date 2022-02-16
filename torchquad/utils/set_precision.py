846bbb756fa3d475992ae2385d28c040f097a1ec
FHof <ga84muv@mytum.de>
2022-02-16 15:30:49 +0100

    Support float32 and float64 in set_precision
    The string "float" can be confusing because in C code it corresponds to a 32 bit number and in Python3 code it corresponds to a 64 bit number.

--- torchquad/utils/set_precision.py
+++ torchquad/utils/set_precision.py
@@ -14,57 +14,7 @@ def _get_precision(backend):
     return os.environ.get(f"TORCHQUAD_DTYPE_{backend.upper()}", None)
 
 
-def _set_precision_torch(data_type="float"):
-    """This function allows the user to set the default precision for torch.
-    Call before declaring your variables.
-
-    Args:
-        data_type (string, optional): Data type to use, either "float" or "double". Defaults to "float".
-
-    """
-    import torch
-
-    if torch.cuda.is_initialized():
-        if data_type.lower() == "double":
-            logger.info(
-                "Setting Torch's default tensor type to cuda.Float64 (CUDA is initialized)."
-            )
-            torch.set_default_tensor_type(torch.cuda.DoubleTensor)
-        elif data_type.lower() == "float":
-            logger.info(
-                "Setting Torch's default tensor type to cuda.Float32 (CUDA is initialized)."
-            )
-            torch.set_default_tensor_type(torch.cuda.FloatTensor)
-        else:
-            logger.error(
-                data_type,
-                "Invalid data type. Only float or double are supported. "
-                "Setting default tensor type of Torch to cuda.Float32 (CUDA is initialized). "
-                "See 'Message' in the line above for the data type used.",
-            )
-            torch.set_default_tensor_type(torch.cuda.FloatTensor)
-    else:
-        if data_type.lower() == "double":
-            logger.info(
-                "Setting Torch's default tensor type to Float64 (CUDA not initialized)."
-            )
-            torch.set_default_tensor_type(torch.DoubleTensor)
-        elif data_type.lower() == "float":
-            logger.info(
-                "Setting Torch's default tensor type to Float32 (CUDA not initialized)."
-            )
-            torch.set_default_tensor_type(torch.FloatTensor)
-        else:
-            logger.error(
-                data_type,
-                "Invalid data type. Only float or double are supported. "
-                "Setting default tensor type of Torch to Float32. "
-                "See 'Message' in the line above for the data type used.",
-            )
-            torch.set_default_tensor_type(torch.FloatTensor)
-
-
-def set_precision(data_type="float", backend="torch"):
+def set_precision(data_type="float32", backend="torch"):
     """This function allows the user to set the default precision for floating point numbers for the given numerical backend.
     Call before declaring your variables.
     Numpy and Tensorflow don't have global dtypes:
@@ -73,23 +23,44 @@ def set_precision(data_type="float", backend="torch"):
     Therefore, torchquad sets the dtype argument for these two when initialising the integration domain.
 
     Args:
-        data_type (string, optional): Data type to use, either "float" or "double". Defaults to "float".
+        data_type (string, optional): Data type to use, either "float32" or "float64". Defaults to "float32".
         backend (string, optional): Numerical backend for which the data type is changed. Defaults to "torch".
     """
-    if backend == "torch":
-        _set_precision_torch(data_type)
-    elif data_type not in ["float", "double"]:
+    # Backwards-compatibility: allow "float" and "double", optionally with
+    # upper-case letters
+    data_type = {"float": "float32", "double": "float64"}.get(
+        data_type.lower(), data_type
+    )
+    if data_type not in ["float32", "float64"]:
         logger.error(
-            f'Invalid data type for {backend}: "{data_type}". Only float or double are supported.'
+            f'Invalid data type "{data_type}". Only float32 and float64 are supported. Setting the data type to float32.'
         )
+        data_type = "float32"
+
+    if backend == "torch":
+        import torch
+
+        cuda_enabled = torch.cuda.is_initialized()
+        tensor_dtype, tensor_dtype_name = {
+            ("float32", True): (torch.cuda.FloatTensor, "cuda.Float32"),
+            ("float64", True): (torch.cuda.DoubleTensor, "cuda.Float64"),
+            ("float32", False): (torch.FloatTensor, "Float32"),
+            ("float64", False): (torch.DoubleTensor, "Float64"),
+        }[(data_type, cuda_enabled)]
+        cuda_enabled_info = (
+            "CUDA is initialized" if cuda_enabled else "CUDA not initialized"
+        )
+        logger.info(
+            f"Setting Torch's default tensor type to {tensor_dtype_name} ({cuda_enabled_info})."
+        )
+        torch.set_default_tensor_type(tensor_dtype)
     elif backend == "jax":
         from jax.config import config
 
-        config.update("jax_enable_x64", data_type == "double")
+        config.update("jax_enable_x64", data_type == "float64")
         logger.info(f"JAX data type set to {data_type}")
     elif backend in ["numpy", "tensorflow"]:
-        dtype_name = {"float": "float32", "double": "float64"}[data_type]
-        os.environ[f"TORCHQUAD_DTYPE_{backend.upper()}"] = dtype_name
+        os.environ[f"TORCHQUAD_DTYPE_{backend.upper()}"] = data_type
         logger.info(
             f"Default dtype config for backend {backend} set to {_get_precision(backend)}"
         )
