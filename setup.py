a302d4f69ef6b748a149a32961d73d4bfc059643
FHof <ga84muv@mytum.de>
2022-02-24 10:55:12 +0100

    Update the package dependency files
    * Replace torch with autoray in the dependencies
    * Move environment.yml to environment_all_backends.yml for the installation of all backends
    * Create a new environment.yml for the installation of minimally required packages

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "matplotlib>=3.3.3",
         "scipy>=1.6.0",
         "tqdm>=4.56.1",
-        "torch>=1.7.1",
+        "autoray>=0.2.5",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
