0c8aef3bd6ce8cd9404480620aad27c9152ed6fe
Erica <elastufka@gmail.com>
2022-04-08 14:37:24 +0200

    transform xi,wi correctly

--- torchquad/integration/gauss_legendre.py
+++ torchquad/integration/gauss_legendre.py
@@ -13,20 +13,21 @@ class GaussLegendre(BaseIntegrator):
         super().__init__()
         
     def _gauss_legendre(self,n):
-        '''returns Gauss-Legendre points and weights for degree n and dimension self._dim'''
-        x,w=np.polynomial.legendre.leggauss(n)
-        xi=np.repeat(x,self._dim).reshape((n,self._dim)).T
-        wi=np.repeat(w,self._dim).reshape((n,self._dim)).T
-        return xi,wy
+        """returns Gauss-Legendre points and weights for degree n and dimension self._dim"""
+        return np.polynomial.legendre.leggauss(n)
 
-    def integrate(self, fn, dim, start_npoints=4, eps_rel=None,
-    eps_abs=None, max_npoints=12,integration_domain=None):
-        """Integrates the passed function on the passed domain using the trapezoid rule.
+    def integrate(self, fn, dim, N=2, eps_abs=None,
+    eps_rel=1e-3, max_N=12, base=2,integration_domain=None):
+        """Integrates the passed function on the passed domain using Gauss-Legendre quadrature.
 
         Args:
             fn (func): The function to integrate over.
             dim (int): Dimensionality of the function to integrate.
-            N (int, optional): Total number of sample points to use for the integration. Defaults to 8.
+            N (int, optional): Total number of sample points to use for the integration. Defaults to 2.
+            eps_abs (float): Absolute error condition used to evaluate quadrature. Defaults to None
+            eps_rel (float): Relative error condition used to evaluate quadrature. Defaults to 1e-3
+            max_N (int, optional): Maximum number of sample points to use for the integration. Defaults to 12.
+            base (int, optional): Base number to use for determining npoints. Defaults to 2. This means if N=2, the number of Gauss-Legendre points that the integral starts with will be base**N or 2**2=4. Likewise, the maximum number of points that the integral will evaluate will be base**max_N, or with the defaults 2**12=4096.
             integration_domain (list, optional): Integration domain, e.g. [[-1,1],[0,1]]. Defaults to [-1,1]^dim.
 
         Returns:
@@ -38,39 +39,45 @@ class GaussLegendre(BaseIntegrator):
             integral=gl.integrate(cosfn,dim=10,eps_rel=1e-10)
         """
         self._integration_domain = _setup_integration_domain(dim, integration_domain)
-        self._check_inputs(dim=dim, N=start_npoints, integration_domain=self._integration_domain) #might need to check more
+        self._check_inputs(dim=dim, N=N, integration_domain=self._integration_domain) #might need to check more
 
         #logger.debug(f"Using Trapezoid for integrating a fn with {npoints} points over {self._integration_domain}")
 
         self._dim = dim
         self._fn = fn
 
-        for ires in range(int(np.log2(start_npoints)), max_npoints + 1): #if starting at npoints=8
-            npoints = 2 ** ires #is this standard?
-            if npoints > 2**max_npoints:
-                raise ValueError(f"Integral did not satisfy the conditions eps_abs={eps_abs} or eps_rel={eps_rel} using the maximum number of points {2**max_npoints}") #or a different error?
+        for ires in range(N, max_N + 1): #if starting at npoints=8
+            npoints = base ** ires #is this standard?
+            #print(f"npoints {npoints}")
+            if npoints > base**max_N:
+                raise ValueError(f"Integral did not satisfy the conditions eps_abs={eps_abs} or eps_rel={eps_rel} using the maximum number of points {base**max_N}") #or a different error?
                 break
 
             # generate positions and weights
-            xi, wi = self._gauss_legendre(npoints)
-            #TO DO: need to scale from [-1,1] to [a,b]
+            xi, wi = self._gauss_legendre(npoints)  #(dim,n)
+            #scale from [-1,1] to [a,b]
+            a,b=self._integration_domain.T
+            xm=0.5*(b+a)
+            xl=0.5*(b-a)
+            xi=np.repeat(xm,npoints).reshape(self._dim,npoints)+np.outer(xl,xi)
+            wi=np.outer(wi,xl).T
             
             logger.debug("Evaluating integrand for {xi}.")
             if self._nr_of_fevals > 0:
                 lastsum = np.array(integral)
-                integral[i] = torch.sum(self._eval(xi)*wi,axis=1) #integral from -1 to 1 f(x) ≈ sum (w_i*f(x_i))
+                integral[i] = torch.sum(self._eval(xi[i])*wi[i],axis=1)
             else:
-                integral = torch.sum(self._eval(xi)*wi,axis=1) #integral from -1 to 1 f(x) ≈ sum (w_i*f(x_i))
+                integral = torch.sum(self._eval(xi)*wi,axis=1) #integral from a to b f(x) ≈ sum (w_i*f(x_i))
 
             print(npoints,integral)
             # Convergence criterion
-            if self._nr_of_fevals//start_npoints > 1:
+            if self._nr_of_fevals//self._dim > 1:
                 l1 = np.abs(integral - lastsum)
                 if eps_abs is not None:
-                    i = np.where(l1 > eps_abs)[0] #does this work in higher dimensions?
+                    i = np.where(l1 > eps_abs)[0]
                 if eps_rel is not None:
                     l2 = eps_rel * np.abs(integral)
-                    i = np.where(l1 > l2)[0] #does this work in higher dimensions?
+                    i = np.where(l1 > l2)[0]
             else:
                 i= np.arange(self._dim) #indices of integral
 
@@ -79,5 +86,4 @@ class GaussLegendre(BaseIntegrator):
                 break
 
         logger.info(f"Computed integral was {integral}.")
-
         return integral
