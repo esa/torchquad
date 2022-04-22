9e196d4f398036c581edb5a2b9a8e53fd63dfee0
Erica <elastufka@gmail.com>
2022-04-22 09:43:54 +0200

    fix import

--- torchquad/__init__.py
+++ torchquad/__init__.py
@@ -13,7 +13,7 @@ from .integration.trapezoid import Trapezoid
 from .integration.simpson import Simpson
 from .integration.boole import Boole
 from .integration.vegas import VEGAS
-from .integration.gauss_legendre import GaussLegendre, GaussJacobi, GaussLaguerre, GaussHermite
+from .integration.gaussian import GaussLegendre, GaussJacobi, GaussLaguerre, GaussHermite
 
 from .plots.plot_convergence import plot_convergence
 from .plots.plot_runtime import plot_runtime
