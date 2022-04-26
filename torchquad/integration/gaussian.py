58d7de79de1f4e045a15bab197a2c5f592f05218
Erica <elastufka@gmail.com>
2022-04-26 14:35:44 +0200

    make sure tensors are on same devicepart 4

--- torchquad/integration/gaussian.py
+++ torchquad/integration/gaussian.py
@@ -47,9 +47,9 @@ class Gaussian(BaseIntegrator):
     def _expand_dimension(self,x):
         '''expand dimensions of 1D array or tensor'''
         try:
-            x=do("repeat",x,self._dim,like="numpy").reshape(self._dim,xi.shape[0])
+            x=do("repeat",x,self._dim,like="numpy").reshape(self._dim,x.shape[0])
         except TypeError: #need tensor to be on cpu, then assign it back to GPU
-            x=do("repeat",x.cpu().detach(),self._dim,like="numpy").reshape(self._dim,xi.shape[0]).to(x.device)
+            x=do("repeat",x.cpu().detach(),self._dim,like="numpy").reshape(self._dim,x.shape[0]).to(x.device)
     
     def integrate(self, fn, dim, args=None, N=8, integration_domain=None):
         """Integrates the passed function on the passed domain using fixed-point Gaussian quadrature.
