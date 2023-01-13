86f5312720a957c5107d41248c95c838fe6dc5a9
ilan-gold <ilanbassgold@gmail.com>
2023-01-13 10:05:52 +0100

    (style): use different variable to prevent overriding argument

--- torchquad/integration/newton_cotes.py
+++ torchquad/integration/newton_cotes.py
@@ -57,12 +57,12 @@ class NewtonCotes(BaseIntegrator):
         # to the end.  Finally we reshape the resulting object so that instead of the last dimension being `dim*N`, it is
         # `N,N,...` as desired.
         einsum = "".join([chr(i + 65) for i in range(len(function_values.shape))])
-        function_values = anp.einsum(f'{einsum}->{einsum[1:]}{einsum[0]}', function_values)
-        function_values = function_values.reshape(new_shape)
-        assert new_shape == list(function_values.shape)
+        reshaped_function_values = anp.einsum(f'{einsum}->{einsum[1:]}{einsum[0]}', function_values)
+        reshaped_function_values = reshaped_function_values.reshape(new_shape)
+        assert new_shape == list(reshaped_function_values.shape)
         logger.debug("Computing areas.")
 
-        result = self._apply_composite_rule(function_values, dim, hs)
+        result = self._apply_composite_rule(reshaped_function_values, dim, hs)
 
         logger.opt(lazy=True).info(
             "Computed integral: {result}", result=lambda: str(result)
