694b724e28c2d7b8014fbbd70d684a61c5a048d7
ilan-gold <ilanbassgold@gmail.com>
2023-03-06 16:26:29 +0100

    (fix): re-compile per integrand because of different shapes

--- torchquad/tests/trapezoid_test.py
+++ torchquad/tests/trapezoid_test.py
@@ -87,11 +87,13 @@ def _run_trapezoid_tests(backend, _precision):
         assert error < 7000
 
     if backend != "numpy":
-        N = 401
+        N = 100000
 
-        integrate = tp.get_jit_compiled_integrate(
-            dim=1, N=N, backend=backend
-        )
+        def integrate(*args, **kwargs):
+            jit_integrate = tp.get_jit_compiled_integrate(
+                dim=1, N=N, backend=backend
+            )
+            return jit_integrate(*args, **kwargs)
         errors, funcs = compute_integration_test_errors(
             integrate,
             {},
@@ -102,9 +104,11 @@ def _run_trapezoid_tests(backend, _precision):
         print(f"1D Trapezoid JIT Test passed. N: {N}, backend: {backend}, Errors: {errors}")
         # Polynomials up to degree 5 can be integrated almost exactly with Boole.
         for err, test_function in zip(errors, funcs):
-            assert test_function.get_order() > 5 or err < 6.33e-11
+            assert test_function.get_order() > 1 or (
+                err < 2e-11 if test_function.is_integrand_1d else err < 5e-10
+            )  # errors add up if the integrand is higher dimensional
         for error in errors:
-            assert error < 6.33e-11
+            assert error < 1e-5
 
 
 test_integrate_numpy = setup_test_for_backend(_run_trapezoid_tests, "numpy", "float64")
