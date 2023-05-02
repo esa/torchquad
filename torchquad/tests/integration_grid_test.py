1a9235e99adcb3dab45a455783daba42ae796b95
ilan-gold <ilanbassgold@gmail.com>
2023-05-02 15:36:05 +0200

    (feat): add basic test

--- torchquad/tests/integration_grid_test.py
+++ torchquad/tests/integration_grid_test.py
@@ -1,12 +1,40 @@
 import sys
+import pytest
+
 
 sys.path.append("../")
 
 from autoray import numpy as anp
 from autoray import to_backend_dtype
-
 from integration.integration_grid import IntegrationGrid
+from integration.grid_integrator import GridIntegrator
+from integration.utils import _linspace_with_grads
+
 from helper_functions import setup_test_for_backend
+
+
+class MockIntegrator(GridIntegrator):
+    def __init__(self, disable_integration_domain_check, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.disable_integration_domain_check = disable_integration_domain_check
+
+    def integrate(self, fn, dim, N, integration_domain, backend, grid_check):
+        grid_points, _, _ = self.calculate_grid(
+            N,
+            integration_domain,
+            disable_integration_domain_check=self.disable_integration_domain_check,
+        )
+        grid_points = grid_points.reshape(N, -1)
+        assert grid_check(grid_points)
+
+    @property  # need to override in order to handle the grid so that we return a multiple 1d grids for each domain in the customized integration_domain
+    def _grid_func(self):
+        def f(integration_domain, N, requires_grad=False, backend=None):
+            b = integration_domain[:, 1]
+            a = integration_domain[:, 0]
+            return _linspace_with_grads(a, b, N, requires_grad=requires_grad)
+
+        return f
 
 
 def _check_grid_validity(grid, integration_domain, N, eps):
@@ -63,6 +91,41 @@ def _run_integration_grid_tests(backend, dtype_name):
         integration_domain = anp.array(dom, dtype=dtype, like=backend)
         grid = IntegrationGrid(N, integration_domain)
         _check_grid_validity(grid, integration_domain, N, eps)
+
+    mock_integrator_no_check = MockIntegrator(disable_integration_domain_check=True)
+    mock_integrator_check = MockIntegrator(disable_integration_domain_check=False)
+
+    # Bypassing check, the output grid should be shape (N, 3) for 3 different 1d domains.
+    # Our custom _grid_func treats the integration_domain as a list of 1d domains
+    # That is why it's shape is (1, 3, 2) so that the IntegrationGrid recognizes it as a 1d integral, but our
+    # custom handler does the rest without the check, and fails with the check.
+    N = 500
+    dim = 1
+
+    def grid_check(x):
+        has_right_shape = x.shape == (N, 3)
+        has_right_vals = anp.all(x[0, :] == 0) and anp.all(x[-1, :] == 1)
+        return has_right_shape and has_right_vals
+
+    mock_integrator_no_check.integrate(
+        lambda x: x,
+        dim,
+        N,
+        anp.array([[[0.0, 1.0], [0.0, 1.0], [0.0, 1.0]]], like=backend),
+        backend,
+        grid_check,
+    )
+    # Without bypassing check, the error raised should be that the input domain is not compatible with the requested dimensions
+    with pytest.raises(ValueError) as excinfo:
+        mock_integrator_check.integrate(
+            lambda x: x,
+            dim,
+            49,
+            anp.array([[[0.0, 1.0], [0.0, 1.0], [0.0, 1.0]]], like=backend),
+            backend,
+            grid_check,
+        )
+    assert "The integration_domain tensor has an invalid shape" == str(excinfo.value)
 
 
 test_integration_grid_numpy = setup_test_for_backend(
